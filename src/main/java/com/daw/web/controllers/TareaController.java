6c544a5dae40bb6dde3683c48555f296dee7ed4a
JAJH <72751999+juann17-afk@users.noreply.github.com>
2025-03-27 12:26:26 +0100

    .

---
+++ src/main/java/com/daw/web/controllers/TareaController.java
@@ -0,0 +1,10 @@
+package com.daw.web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tareas")
+public class TareaController {
+
+}
