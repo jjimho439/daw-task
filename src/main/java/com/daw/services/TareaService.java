7565b15376bd5ab084ea8c11d400bb05b901e34f
jjimho439 <fantitasingas@gmail.com>
2025-03-27 13:45:45 +0100

    .

--- src/main/java/com/daw/services/TareaService.java
+++ src/main/java/com/daw/services/TareaService.java
@@ -1,5 +1,26 @@
 package com.daw.services;
 
-public class TareaService {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.daw.persistence.entities.Tarea;
+import com.daw.persistence.entities.enums.Estado;
+
+
+@Service
+public class TareaService {
+	
+	//@Autowired
+	// private TareaRepository tareaRepository
+	
+	
+	 public List <Tarea> findAll(){
+		 List<Tarea> listaTareas = new ArrayList<>();
+			
+	        //return this.TareaRepository.findAll();
+		 return null;
+	 }
 }
